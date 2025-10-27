460f3d37444fe807bcde2e18277a97a6fb43a843
Grenz1inie <3023225096@qq.com>
2025-10-27 21:12:53 +0800

    wu

--- SnakeGameWithAI/obj/Debug/net8.0/SnakeGameWithAI.AssemblyInfo.cs
+++ SnakeGameWithAI/obj/Debug/net8.0/SnakeGameWithAI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SnakeGameWithAI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3214735733eea20dc2476f4d3ad3a94be6d49277")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9801ee79611cbb9796477ac00540691f755cea3")]
 [assembly: System.Reflection.AssemblyProductAttribute("SnakeGameWithAI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SnakeGameWithAI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
